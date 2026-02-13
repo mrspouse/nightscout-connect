181a4ef525a1eb6fe2b307eb9d6bbcf7f7426d0b
mrspouse <55619185+mrspouse@users.noreply.github.com>
2026-02-13 11:13:41 +0000

    Add puppeteer based auth

--- glooko-login.js
+++ glooko-login.js
@@ -3,7 +3,9 @@
 /*
 * 
 * https://github.com/nightscout/nightscout-connect/issues/14#issuecomment-3239520325
-*
+* Lorenzo Sandini
+* Uses Puppeteer to simulate real browser authentication
+* 
 */
 
 const puppeteer = require('puppeteer');
